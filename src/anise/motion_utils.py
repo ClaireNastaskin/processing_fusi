5d3cebfb756a043e6cd812a9b693b34f7a0a1e56
ewinapun <ewinatkpun@gmail.com>
2024-07-24 15:53:31 -0700

    rename utils filename

--- src/anise/motion_utils.py
+++ src/anise/motion_utils.py
@@ -8,12 +8,6 @@ from numpy.fft import fft2, ifft2, fftshift
 from scipy.ndimage import shift
 from scipy import ndimage
 
-import nibabel as nib
-import nilearn as nl
-from nilearn.glm.first_level import make_first_level_design_matrix
-from nilearn.plotting import plot_design_matrix
-from nilearn import plotting
-
 # from anise.io.load_matlab_dataset import load_data, load_selected_data
 import SimpleITK as sitk
 import ants
@@ -83,6 +77,16 @@ def plot_pd_intensity_over_time(imgs, y_lim=[], outlier_threshold=0):
     plt.show()
     return doppler_intensity, outlier_intensity_frames, fig, ax
 
+def boxcar_smooth(data, window_size):
+    # smooth across frames with a boxcar moving average
+    assert window_size>0, "Window size must be greater than 0"
+    smoothed_data = np.copy(data)
+    for t in range(data.shape[-1]):
+        start_index = max(0, t - window_size // 2)
+        end_index = min(data.shape[-1], t + window_size // 2 + 1)
+        smoothed_data[..., t] = np.mean(data[..., start_index:end_index], axis=-1)
+    return smoothed_data
+# phase correlation related
 def phase_corr_translation(im1, im2):
     
     """ performs FFT phase correlation to find optimal translation between two images
@@ -265,7 +269,7 @@ def register_ants(images, type_of_transform='Rigid', ref_index=0, crop_border=0)
 
     # Initialize transformations array with zero for the first image (identity transformation)
     extra = []
-    for i in tqdm(range(images.shape[-1])):
+    for i in (range(images.shape[-1])):
         moving_image = ants.from_numpy(images[..., i])
         result = ants.registration(fixed=fixed_image, 
                                    moving=moving_image, 
@@ -326,15 +330,17 @@ def crop_images(images, crop_border):
         transformed_images.append(cropped_image)
     return np.stack(transformed_images, axis=-1)
 
-def plot_transform_params(extra, oop_labels=[], metric_name='', in_plane_indices=[], 
+def plot_transform_params(extra, oop_labels=[], metric_name='', 
                             annotate_ref_frame: int = None):
     # Create a new figure with three subplots
     # similarity metric value
     # translation after registration
     # rotation after registration
 
-    if not any(in_plane_indices):
+    if "in_plane_indices" not in extra:
         in_plane_indices = range(len(extra))
+    else:
+        in_plane_indices = extra['in_plane_indices']
     fig, axs = plt.subplots(3, 1,figsize=(8,8))
 
     # get the range of oop start stops for plotting
@@ -346,23 +352,23 @@ def plot_transform_params(extra, oop_labels=[], metric_name='', in_plane_indices
     def add_shaded_oop_indices(ax, oop_labels):
     # add shaded region for out-of-plane frames
         if len(range_oop) > 0:
-            ax.axvspan(range_oop[0], range_oop[1], color='k', alpha=0.2, label=f'Discarded frames')
+            ax.axvspan(range_oop[0], range_oop[1], color='k', alpha=0.2, label=f'discard frames')
             for i in range(2, len(range_oop)-1, 2):
                 ax.axvspan(range_oop[i], range_oop[i+1], color='k', alpha=0.2)
 
-    def add_annotation_ref_frame(ax, annotate_ref_frame, y):
+    def add_annotation_ref_frame(ax, in_plane_indices, annotate_ref_frame, y):
         if annotate_ref_frame is not None:
-            ax.scatter(annotate_ref_frame, y[annotate_ref_frame], color='magenta', 
-                        label=f'reference frames: {annotate_ref_frame}')
+            ax.scatter(in_plane_indices[annotate_ref_frame], y[annotate_ref_frame], color='magenta', 
+                        label=f'reference frame')
 
     ax = axs[0]
-    ax.plot(in_plane_indices, extra['initial_similarity'], label='initial_similarity')
-    ax.plot(in_plane_indices, extra['after_similarity'], label='after_similarity')
+    ax.plot(in_plane_indices, extra['initial_similarity'], label='initial similarity')
+    ax.plot(in_plane_indices, extra['after_similarity'], label='after similarity')
     before = extra['initial_similarity'].mean()
     after = extra['after_similarity'].mean()
     improvement = (after - before) / before * 100
     add_shaded_oop_indices(ax, range_oop)
-    add_annotation_ref_frame(ax, annotate_ref_frame, extra['after_similarity'])
+    add_annotation_ref_frame(ax, in_plane_indices, annotate_ref_frame, extra['after_similarity'])
     ax.set_title(f'Similarity Metric: {metric_name}, {improvement:.2f}% improvement')
     ax.set_ylabel('Similarity Metric')
     ax.legend()
@@ -372,7 +378,7 @@ def plot_transform_params(extra, oop_labels=[], metric_name='', in_plane_indices
     ax.plot(in_plane_indices, extra['translation_x'], label='x')
     ax.plot(in_plane_indices, extra['translation_y'], label='y')
     add_shaded_oop_indices(ax, range_oop)
-    add_annotation_ref_frame(ax, annotate_ref_frame, extra['translation_x'])
+    add_annotation_ref_frame(ax, in_plane_indices, annotate_ref_frame, extra['translation_x'])
     ax.set_title('Translation')
     ax.set_ylabel('pixel')
     lim = max(np.abs(extra['translation_x']).max(), np.abs(extra['translation_y']).max(), 5)
@@ -383,7 +389,7 @@ def plot_transform_params(extra, oop_labels=[], metric_name='', in_plane_indices
     ax = axs[2]
     ax.plot(in_plane_indices, extra['rotation'], label='rotation')
     add_shaded_oop_indices(ax, range_oop)
-    add_annotation_ref_frame(ax, annotate_ref_frame, extra['rotation'])
+    add_annotation_ref_frame(ax, in_plane_indices, annotate_ref_frame, extra['rotation'])
     ax.set_title('Rotation')
     ax.set_xlabel('Frame Index')
     ax.set_ylabel('Angle')
@@ -393,16 +399,6 @@ def plot_transform_params(extra, oop_labels=[], metric_name='', in_plane_indices
     ax.grid(True)
     plt.tight_layout()
     return fig, axs, improvement
-
-def boxcar_smooth(data, window_size):
-    # smooth across frames with a boxcar moving average
-    assert window_size>0, "Window size must be greater than 0"
-    smoothed_data = np.copy(data)
-    for t in range(data.shape[-1]):
-        start_index = max(0, t - window_size // 2)
-        end_index = min(data.shape[-1], t + window_size // 2 + 1)
-        smoothed_data[..., t] = np.mean(data[..., start_index:end_index], axis=-1)
-    return smoothed_data
 
 def check_weird_transform(extra, max_translation_limit=10, angle_limit=0.1):
     # check if the transformation is not smooth based on the difference between consecutive frames
@@ -419,6 +415,44 @@ def check_weird_transform(extra, max_translation_limit=10, angle_limit=0.1):
         print(f'Weird transformation detected with {outlier_a:.1f} radian in rotation.')
         return True
     return False
+
+def auto_ants_registration_on_in_plane(data, in_plane_indices, ref_frame_list=[], optimize_ref=True):
+    # register in-plane images ONLY
+    data_selected = data[:, :, in_plane_indices]
+    # initialize the ref_frame to the point closest to the centroid of the in-plane cluster
+    if any(ref_frame_list):
+        ref_frame = ref_frame_list[0]
+    else:
+        # use the first in-plane image as the reference frame
+        ref_frame = final_ref_frame = 0
+
+    # register the images
+    registered_images, extra = register_ants(data_selected, ref_index=ref_frame)
+    extra['in_plane_indices'] = in_plane_indices
+    # check if the transformation is weird (e.g. rotation > 10 degrees)
+    weird_transform_flag = check_weird_transform(extra)
+    i = 1
+    if optimize_ref:
+        while weird_transform_flag:
+            ref_frame = ref_frame_list[i]
+            print(f'Re-registering images...')
+            registered_images, extra = register_ants(data_selected, ref_index=ref_frame)
+            weird_transform_flag = check_weird_transform(extra)
+            if i < len(ref_frame_list):
+                i += 1
+            else:
+                break
+        if not weird_transform_flag:
+            extra['in_plane_indices'] = in_plane_indices
+            final_ref_frame = ref_frame
+            print('Done. Transformation was successfully performed.')
+        else: 
+            registered_images = data_selected
+            extra = None
+            final_ref_frame = None
+            print('No suitable transformation was performed. Revert to no registration.')
+    
+    return registered_images, extra, final_ref_frame
 
 # cluster related
 def get_consecutive_labels_counts(labels):
@@ -518,3 +552,86 @@ def plot_embedding(embedding, labels=None, title='', legend='on', centroids=[]):
     plt.xticks([])
     plt.yticks([])
     return fig, ax
+
+def dim_red(fusi_data, param_c):
+    dim_red_method = param_c["dim_red_method"] # 'VGG' or 'PCA' or 'UMAP' or 'tSNE'
+    pca_n_components = param_c["pca_n_components"]
+    
+    print(f'Using {dim_red_method} for clustering and visualization...')
+
+    if dim_red_method == 'VGG':
+        # Apply VGG to extract features, then PCA on the features
+        from VGG_feature_extraction import VGG_PCA
+        embedding = VGG_PCA(fusi_data, pca_n_components=2)
+    else:
+        # use the thresholded images 
+        threshold_imgs, density = get_threshold_image(fusi_data, threshold=0.5)
+        print('Applied binary masking to get thresholded images')
+        show_imgs(threshold_imgs, np.arange(20), fig_height=1.1, title='off')
+        imgs_flatten = threshold_imgs.reshape(-1, threshold_imgs.shape[-1]).T
+        # imgs_flatten = fusi_data.reshape(-1, fusi_data.shape[-1]).T
+
+        from sklearn.decomposition import PCA
+        pca=PCA(n_components=pca_n_components)
+
+        # Reduce number of dimension
+        lowd = pca.fit_transform(imgs_flatten)
+        print(f'After PCA shape: {lowd.shape}')
+        explained_variance = pca.explained_variance_ratio_
+        print(f'Explain variance sum of top {pca_n_components} components: \
+            {sum(explained_variance).round(3)*100}%')
+
+        if dim_red_method == 'PCA':
+            ### Visualize with PCA
+            embedding = lowd[:,:2]
+            
+        elif dim_red_method == 'UMAP':
+            import umap
+            ### Visualize PCA projection with UMAP
+            reducer = umap.UMAP(random_state=42, metric='cosine')
+            embedding = reducer.fit_transform(lowd)
+
+        elif dim_red_method == 'tSNE':
+            from sklearn.manifold import TSNE
+            ### Visualize PCA projection with tSNE
+            tsne = TSNE(n_components=2, verbose=0, 
+                        perplexity=param_c["perplexity"], max_iter=300, random_state=42)
+            embedding = tsne.fit_transform(lowd)
+
+    # Visualize top 2 embedding space
+    plot_embedding(embedding, title=(dim_red_method + ' projection'))
+
+    return embedding
+
+def dbscan_clustering(embedding, starting_eps=1):
+    from sklearn.cluster import DBSCAN
+    # normalize the embedding
+    embedding = embedding / np.std(embedding, axis=0)
+    n_clusters = 5
+    n_noise = embedding.shape[0]
+    eps = starting_eps
+    while n_clusters > 4 or n_noise > embedding.shape[0] / 2:
+        print(f'Using DBSCAN with eps = {eps}\n')
+        db = DBSCAN(eps=eps).fit(embedding)
+        labels = db.labels_
+
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise = list(labels).count(-1)
+
+        print("Estimated number of clusters: %d" % n_clusters)
+        print("Estimated number of noise points: %d" % n_noise)
+        eps += 0.1
+    return labels, n_clusters, n_noise, eps
+
+def sort_in_plane_points_closest_to_centroid(embedding, pred_labels, n_clusters):
+    # Find nearest point to the centroid of the clusters
+    centroids = np.zeros((n_clusters, embedding.shape[-1]))
+    for cluster in range(n_clusters):
+        cluster_indices = np.where(pred_labels == cluster)[0]
+        centroids[cluster] = np.mean(embedding[cluster_indices], axis=0)
+        distance = np.linalg.norm(centroids[cluster].reshape(1,-1) - embedding, axis=1)
+        closest_point_sorted = np.argsort(distance[cluster_indices])
+        if cluster == 0: # in-plane
+            closest_points_in_plane = closest_point_sorted
+    return centroids, closest_points_in_plane
