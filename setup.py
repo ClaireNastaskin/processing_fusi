b52306e1c4108bc27059bab7fe82a5d300c7b3eb
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2024-12-27 11:09:02 -0800

    Remove unused setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-# install anise module for import in notebooks and other code
-setup(
-    name='anise',
-    version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-)
